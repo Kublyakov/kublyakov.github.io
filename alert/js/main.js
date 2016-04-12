37abfe9771e0f6fbfdf7d456dd86799aa86afa45
Kublyakov <kublyakovkirill@gmail.com>
2016-04-12 15:53:11 +0300

    Add numbers

--- alert/js/main.js
+++ alert/js/main.js
@@ -16,19 +16,19 @@ test.changetype();
 let ua = detect.parse(navigator.userAgent);
 
 document.write (
-  '1' + ua.device.family + '<br>',
-  '2' + ua.device.name + '<br>',
-  '3' + ua.device.version + '<br>',
-  '4' + ua.device.major + '<br>',
-  '5' + ua.device.minor + '<br>',
-  '6' + ua.device.patch + '<br>',
-  '7' + ua.device.type + '<br>',
-  '8' + ua.device.manufacturer + '<br>',
+  '1: ' + ua.device.family + '<br>',
+  '2:  ' + ua.device.name + '<br>',
+  '3: ' + ua.device.version + '<br>',
+  '4: ' + ua.device.major + '<br>',
+  '5: ' + ua.device.minor + '<br>',
+  '6: ' + ua.device.patch + '<br>',
+  '7: ' + ua.device.type + '<br>',
+  '8: ' + ua.device.manufacturer + '<br>',
 
-  '9' + ua.os.family + '<br>',
-  '10' + ua.os.name + '<br>',
-  '11' + ua.os.version + '<br>',
-  '12' + ua.os.major + '<br>',
-  '13' + ua.os.minor + '<br>',
-  '14' + ua.os.patch + '<br>'
+  '9: ' + ua.os.family + '<br>',
+  '10: ' + ua.os.name + '<br>',
+  '11: ' + ua.os.version + '<br>',
+  '12: ' + ua.os.major + '<br>',
+  '13: ' + ua.os.minor + '<br>',
+  '14: ' + ua.os.patch + '<br>'
 );
