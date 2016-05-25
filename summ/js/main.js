9338a2a6fc60da9f4b9ef5ca82f6f37fdc8c1e6b
Kublyakov <kublyakovkirill@gmail.com>
2016-05-25 15:59:38 +0300

    Добавил стартовые файлы

---
+++ summ/js/main.js
@@ -0,0 +1,12 @@
+// var num = +prompt('Введите первое число', '');
+// var num2 = +prompt('Введите второе число', '');
+// var summ = +prompt('Введите сумму', '');
+// var taxSumm = +prompt('Введите сумму с налогами', '');
+
+// numPercent = num / summ * 100;
+// numPercent2 = num2 / summ * 100;
+
+// total = taxSumm / 100 * numPercent;
+// total2 = taxSumm / 100 * numPercent2;
+
+// console.log(total.toFixed(2), total2.toFixed(2));
