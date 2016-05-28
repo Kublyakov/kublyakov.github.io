c5deb711e7ef820fd1984eaa54579a7ac6e4e0af
Kublyakov Kirill <kublyakovkirill@gmail.com>
2016-05-28 14:32:48 +0300

    Add basic functional

--- json/js/main.js
+++ json/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-fetch('http://newth.orbitum.com/api/viewtheme.php?std_out=20&oft=0&cat=3')
+fetch('//newth.orbitum.com/api/viewtheme.php?std_out=20&oft=0&cat=3')
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -8,10 +8,34 @@ fetch('http://newth.orbitum.com/api/viewtheme.php?std_out=20&oft=0&cat=3')
           response.status);
         return;
       }
-
-      // Examine the text in the response
       response.json().then(function(data) {
-        console.log(data);
+        var container = document.getElementById('container');
+
+        function themesLoop() {
+          for (var i = 0; i < data.themes.length; i++) {
+            var item = data.themes[i];
+            var itemLink = item.category_id + '/' + item.id;
+
+            var themeItem = `
+              <div class="theme-item">
+                <a href="//newth.orbitum.com/#!cat/${itemLink}" class="image-link">
+                  <img src="${item.grid_preview}" alt="${item.full_title}">
+                </a>
+                <a href="//newth.orbitum.com/#!cat/${itemLink}" class="title-link">
+                  <h2 class="theme-title">
+                    ${item.title}
+                  </h2>
+                </a>
+                <div class="rating">Рейтинг темы: ${item.rating}</div>
+                <div class="install-count">Количество установок: ${item.install_cnt}</div>
+              </div>
+              `;
+            container.insertAdjacentHTML('beforeEnd', themeItem);
+          }
+        }
+
+        themesLoop();
+
       });
     }
   )
