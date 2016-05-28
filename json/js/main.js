f6e45debd9b38226c38f46bd076e7851efb0e117
kublyakov <kublyakovkirill@gmail.com>
2016-05-28 10:22:41 +0300

    Add fetch

--- json/js/main.js
+++ json/js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+fetch('http://newth.orbitum.com/api/viewtheme.php?std_out=20&oft=0&cat=3')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
