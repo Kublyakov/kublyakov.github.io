bb16c1948090f03830590e068ca6ffdaf09adc9c
Kublyakov <kublyakovkirill@gmail.com>
2016-06-20 16:22:50 +0300

    Add some calc logic

--- calc/js/main.js
+++ calc/js/main.js
@@ -1,34 +1,78 @@
 'use strict';
 
-function showCalc() {
-  let chooseContainer = document.getElementById('chooseContainer');
+var chooseContainer = document.getElementById('chooseContainer');
+var proportionCalcBtn = document.getElementById('proportionCalcBtn');
+var percentCalcBtn = document.getElementById('percentCalcBtn');
+var proportionCalcContainer = document.getElementById('proportionCalcContainer');
+var percentCalcContainer = document.getElementById('percentCalcContainer');
+var calcName = document.getElementById('calcName');
+var container = document.getElementById('container');
+var BTN = 13;
 
-  chooseContainer.addEventListener('click', function (e) {
-    let target = e.target;
-    let proportionCalcBtn = document.getElementById('proportionCalcBtn');
-    let percentCalcBtn = document.getElementById('percentCalcBtn');
-    let proportionCalcContainer = document.getElementById('proportionCalcContainer');
-    let percentCalcContainer = document.getElementById('percentCalcContainer');
-    let calcName = document.getElementById('calcName');
+var showCalc = {
+  proportion: function () {
+    proportionCalcContainer.style.display = 'block';
+    percentCalcContainer.style.display = 'none';
+    proportionCalcBtn.classList.add('active');
+    calcName.innerHTML = 'Калькулятор долей';
+    if (localStorage.getItem('calcType') !== 'Proportion') {
+      localStorage.setItem('calcType', 'Proportion');
+      percentCalcBtn.classList.remove('active');
+    }
+  },
+  percent: function () {
+    percentCalcContainer.style.display = 'block';
+    proportionCalcContainer.style.display = 'none';
+    percentCalcBtn.classList.add('active');
+    calcName.innerHTML = 'Калькулятор процентов';
+    if (localStorage.getItem('calcType') !== 'Percent') {
+      localStorage.setItem('calcType', 'Percent');
+      proportionCalcBtn.classList.remove('active');
+    }
+  }
+};
 
-    if (target === proportionCalcBtn) {
-      proportionCalcContainer.style.display = 'block';
-      percentCalcContainer.style.display = 'none';
-      calcName.innerHTML = 'Калькулятор долей';
-    }
-    else if (target === percentCalcBtn) {
-      percentCalcContainer.style.display = 'block';
-      proportionCalcContainer.style.display = 'none';
-      calcName.innerHTML = 'Калькулятор процентов';
-    }
-    else {
-      return false;
-    }
-  })
+if (localStorage.getItem('calcType') === 'Proportion') {
+  showCalc.proportion();
 }
 
-if(!localStorage.getItem('calc')) {
-  console.log('No calc');
-} else {
-  console.log('calc');
+else if (localStorage.getItem('calcType') === 'Percent') {
+  showCalc.percent();
 }
+
+chooseContainer.addEventListener('click', function (e) {
+  var target = e.target;
+
+  if (target === proportionCalcBtn) {
+    showCalc.proportion();
+  }
+  else if (target === percentCalcBtn) {
+    showCalc.percent();
+  }
+  else {
+    return false;
+  }
+});
+
+var input = {
+  item: function () {
+    var elem = document.createElement('input');
+    elem.type = 'text';
+    elem.className = 'num-input form-control';
+    elem.placeholder = 'Введите число';
+    elem.tabIndex = 1;
+    container.appendChild(elem);
+    elem.focus();
+  }
+};
+
+container.addEventListener('keydown', function (e) {
+  var target = e.target;
+  if (target.tagName.toLowerCase() === 'input' && event.keyCode === BTN && target.value > 0) {
+    input.item();
+    target.classList.remove('error-input');
+  }
+  else if (target.tagName.toLowerCase() === 'input' && event.keyCode === BTN && !(target.value > 0)) {
+    target.classList.add('error-input');
+  }
+});
--- js-tasks/js/main.js
+++ js-tasks/js/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var item = document.getElementsByClassName('tutorial-map__link');
-
-for (var i = 0; i < item.length; i++) {
-  if (+(item[i].href.indexOf('/task/')) === 26 ) {
-    console.log(item[i].href);
+function addLinks() {
+  var item = document.querySelectorAll('.tutorial-map__link');
+  for (var i = 0; i < item.length; i++) {
+    if (+(item[i].href.indexOf('/task/')) === 26 ) {
+      return item[i];
+    }
   }
 }
