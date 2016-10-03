ed59351a7d7a0f08df37bcf06cd9294cb66f4502
kublyakov <kublyakovkirill@gmail.com>
2016-10-03 21:51:57 +0300

    Add first version of ultimate calc

--- ultimateCalc/js/main.js
+++ ultimateCalc/js/main.js
@@ -1,15 +1,14 @@
 'use strict';
+let localPercent = localStorage.getItem('percent');
+let percentInput = document.querySelector('.percent');
+let percentBtn = document.querySelector('.percent-btn');
+let percentInputValue = percentInput.value;
 
 const percentError = () => {
-  console.log('error');
+  alert('Ааа, всё сломалось!');
 };
 
 const currentPercent = () => {
-  let percentInput = document.querySelector('.percent');
-  let percentBtn = document.querySelector('.percent-btn');
-  let localPercent = localStorage.getItem('percent');
-  let percentInputValue = percentInput.value;
-
   if (localPercent > 0) {
     percentInput.value = localPercent;
   }
@@ -21,14 +20,32 @@ const currentPercent = () => {
     percentInputValue = percentInput.value.replace(/,/g, '.');
     if (percentInputValue > 0) {
       localStorage.setItem('percent', percentInputValue);
+      location.reload();
     }
     else {
       percentError();
     }
   });
 };
-
 currentPercent();
+
+const calc = () => {
+  let gettingNumber = document.querySelector('.getting-number');
+  let resultNumber = document.querySelector('.result-number');
+
+  gettingNumber.addEventListener('keydown', (e) => {
+    let target = e.target;
+    let num = +(target.value);
+    if (num > 0 && e.keyCode === 13) {
+      let subresult = num + (+num / 100 * 18);
+      let result = subresult + (subresult / 100 * localPercent);
+      resultNumber.value = result.toFixed(2);
+      target.select();
+    }
+  });
+}
+
+calc();
 
 /*
 * Вводим число с необходимыми процентами.
