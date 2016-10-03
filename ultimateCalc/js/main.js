1e6ef9e25c6ec896fef07f8b8b790836bf55d32f
Kirill Kublyakov <kublyakovkirill@gmail.com>
2016-10-03 18:29:46 +0300

    initial ultimateCalc

--- ultimateCalc/js/main.js
+++ ultimateCalc/js/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const setPercent = () => {
+  let percentInput = document.querySelector('.percent');
+  let percentBtn = document.querySelector('.percent-btn');
+  let currentPercent = localStorage.getItem('percent');
+
+  if (currentPercent > 0) {
+    percentInput.value = currentPercent;
+  }
+
+  percentBtn.addEventListener('click', () => {
+    if (percentInput.value > 0) {
+      console.log(percentInput.value);
+      localStorage.setItem('percent', percentInput.value);
+    }
+  });
+}; 
+
+setPercent();
+
+/*
+* Вводим число с необходимыми процентами.
+* Запоминаем число и подставляем его в инпут.
+* Предыдущие значения выводим в отдельную таблицу.
+* При клике на них они подставляются в инпут.
+* Таблица сортируется по давности ввода значений.
+* */
