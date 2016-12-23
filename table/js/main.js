4a959903ac21765ea08bf95e6a3470a4e17e9664
Kirill Kublyakov <kublyakovkirill@gmail.com>
2016-12-23 12:57:09 +0300

    Fix adding tr logic

--- table/js/main.js
+++ table/js/main.js
@@ -6,28 +6,34 @@ const mainTable  = document.querySelector('#main-table');
 const mainTbody  = document.querySelector('#main-tbody');
 
 buildTable.addEventListener('click', () => {
-  let data = inputData.value;
-  let arrLine = data.split(/\n/g);
-  let lineCount = arrLine.length;
+  if (inputData.value !== '') {
+    let data = inputData.value;
+    let arrLine = data.split(/\n/g);
+    let lineCount = arrLine.length;
 
-  for(let i = 0; i < lineCount; i++){
-    let tr = document.createElement('tr');
-    let cell = arrLine[i].split('	');
-    let cellCount = cell.length;
-    for (let j = 0; j < cellCount; j++) {
-      if (j === 3 ||
+    for(let i = 0; i < lineCount; i++){
+      let tr = document.createElement('tr');
+      let cell = arrLine[i].split('	');
+      let cellCount = cell.length;
+      for (let j = 0; j < cellCount; j++) {
+        if (j === 3 ||
           j === 4 ||
           j === 5 ||
           j === 6 ||
           cell.length < 7 ||
           cell[7].length === 0
-      ) continue;
-      let td = document.createElement('td');
-      let item = cell[j];
-      td.innerHTML = item;
-      tr.appendChild(td);
+        ) continue;
+        let td = document.createElement('td');
+        let item = cell[j];
+        td.innerHTML = item;
+        tr.appendChild(td);
+        mainTbody.appendChild(tr);
+      }
     }
-    mainTbody.appendChild(tr);
+    mainTable.appendChild(mainTbody);
   }
-  mainTable.appendChild(mainTbody);
+
+  else {
+    alert('Вставьте данные в поле!');
+  }
 });
